571115e12d7b43159081d62a443a806bac3568c5
Carlos <carlos42044@gmail.com>
2017-07-06 12:07:13 -0400

    Added functionality for adding items, clearing, paying, processing payment, and storing transaction. UI Updates as well.

--- WInFormsMockPOS/Form1.Designer.cs
+++ WInFormsMockPOS/Form1.Designer.cs
@@ -29,48 +29,45 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
             this.username = new System.Windows.Forms.Label();
             this.date = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.required = new System.Windows.Forms.Label();
+            this.tendered = new System.Windows.Forms.TextBox();
+            this.pay = new System.Windows.Forms.Button();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.process = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
-            this.flowLayoutPanel1.SuspendLayout();
             this.flowLayoutPanel2.SuspendLayout();
             this.flowLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(105, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Pay";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(6, 35);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(409, 215);
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(446, 215);
             this.dataGridView1.TabIndex = 1;
             // 
             // tabControl1
@@ -81,7 +78,7 @@
             this.tabControl1.Location = new System.Drawing.Point(3, 19);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(426, 279);
+            this.tabControl1.Size = new System.Drawing.Size(460, 279);
             this.tabControl1.TabIndex = 2;
             // 
             // tabPage1
@@ -91,7 +88,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(418, 250);
+            this.tabPage1.Size = new System.Drawing.Size(452, 250);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Cart";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -102,45 +99,49 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(418, 250);
+            this.tabPage2.Size = new System.Drawing.Size(452, 250);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Trans";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // dataGridView2
             // 
+            this.dataGridView2.AllowUserToAddRows = false;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView2.Location = new System.Drawing.Point(0, 0);
             this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(418, 253);
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.Size = new System.Drawing.Size(456, 253);
             this.dataGridView2.TabIndex = 0;
             // 
-            // button2
+            // add
             // 
-            this.button2.Location = new System.Drawing.Point(3, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Add";
-            this.button2.UseVisualStyleBackColor = true;
+            this.add.Location = new System.Drawing.Point(3, 3);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 2;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // clear
             // 
-            this.button3.Location = new System.Drawing.Point(84, 3);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Clear";
-            this.button3.UseVisualStyleBackColor = true;
+            this.clear.Location = new System.Drawing.Point(84, 3);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 3;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
             // 
             // username
             // 
             this.username.AutoSize = true;
             this.username.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.username.Location = new System.Drawing.Point(3, 0);
             this.username.Name = "username";
-            this.username.Size = new System.Drawing.Size(45, 16);
+            this.username.Size = new System.Drawing.Size(51, 16);
             this.username.TabIndex = 3;
             this.username.Text = "label1";
             this.username.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -151,46 +152,11 @@
             this.date.AutoSize = true;
             this.date.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.date.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.date.Location = new System.Drawing.Point(54, 0);
+            this.date.Location = new System.Drawing.Point(60, 0);
             this.date.Name = "date";
             this.date.Size = new System.Drawing.Size(45, 16);
             this.date.TabIndex = 4;
             this.date.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(3, 6);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 16);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // label3
-            // 
-            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(54, 6);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 16);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Controls.Add(this.label2);
-            this.flowLayoutPanel1.Controls.Add(this.label3);
-            this.flowLayoutPanel1.Controls.Add(this.button1);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 304);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(322, 32);
-            this.flowLayoutPanel1.TabIndex = 7;
             // 
             // flowLayoutPanel2
             // 
@@ -200,13 +166,13 @@
             this.flowLayoutPanel2.Controls.Add(this.flowLayoutPanel1);
             this.flowLayoutPanel2.Location = new System.Drawing.Point(1, 1);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(459, 337);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(522, 337);
             this.flowLayoutPanel2.TabIndex = 8;
             // 
             // flowLayoutPanel3
             // 
-            this.flowLayoutPanel3.Controls.Add(this.button2);
-            this.flowLayoutPanel3.Controls.Add(this.button3);
+            this.flowLayoutPanel3.Controls.Add(this.add);
+            this.flowLayoutPanel3.Controls.Add(this.clear);
             this.flowLayoutPanel3.Location = new System.Drawing.Point(3, 3);
             this.flowLayoutPanel3.Name = "flowLayoutPanel3";
             this.flowLayoutPanel3.Size = new System.Drawing.Size(218, 26);
@@ -217,11 +183,107 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // required
+            // 
+            this.required.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.required.AutoSize = true;
+            this.required.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.required.Location = new System.Drawing.Point(235, 6);
+            this.required.Name = "required";
+            this.required.Size = new System.Drawing.Size(58, 16);
+            this.required.TabIndex = 8;
+            this.required.Text = "required";
+            this.required.Visible = false;
+            // 
+            // tendered
+            // 
+            this.tendered.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tendered.Location = new System.Drawing.Point(171, 4);
+            this.tendered.Name = "tendered";
+            this.tendered.Size = new System.Drawing.Size(58, 20);
+            this.tendered.TabIndex = 7;
+            this.tendered.Text = "0.00";
+            this.tendered.Visible = false;
+            this.tendered.TextChanged += new System.EventHandler(this.tendered_TextChanged);
+            // 
+            // pay
+            // 
+            this.pay.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pay.Location = new System.Drawing.Point(90, 3);
+            this.pay.Name = "pay";
+            this.pay.Size = new System.Drawing.Size(75, 23);
+            this.pay.TabIndex = 0;
+            this.pay.Text = "Pay";
+            this.pay.UseVisualStyleBackColor = true;
+            this.pay.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(48, 6);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(36, 16);
+            this.totalLabel.TabIndex = 6;
+            this.totalLabel.Text = "0.00";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Total";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Controls.Add(this.totalLabel);
+            this.flowLayoutPanel1.Controls.Add(this.pay);
+            this.flowLayoutPanel1.Controls.Add(this.tendered);
+            this.flowLayoutPanel1.Controls.Add(this.required);
+            this.flowLayoutPanel1.Controls.Add(this.process);
+            this.flowLayoutPanel1.Controls.Add(this.cancel);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 304);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(519, 32);
+            this.flowLayoutPanel1.TabIndex = 7;
+            // 
+            // process
+            // 
+            this.process.Enabled = false;
+            this.process.Location = new System.Drawing.Point(299, 3);
+            this.process.Name = "process";
+            this.process.Size = new System.Drawing.Size(75, 23);
+            this.process.TabIndex = 9;
+            this.process.Text = "Process";
+            this.process.UseVisualStyleBackColor = true;
+            this.process.Visible = false;
+            this.process.Click += new System.EventHandler(this.process_Click);
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(380, 3);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 10;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Visible = false;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(463, 339);
+            this.ClientSize = new System.Drawing.Size(525, 339);
             this.Controls.Add(this.flowLayoutPanel2);
             this.Name = "Form1";
             this.Text = "WinFormsMockPos";
@@ -231,33 +293,36 @@
             this.tabPage1.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
-            this.flowLayoutPanel1.ResumeLayout(false);
-            this.flowLayoutPanel1.PerformLayout();
             this.flowLayoutPanel2.ResumeLayout(false);
             this.flowLayoutPanel2.PerformLayout();
             this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button add;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.Label username;
         private System.Windows.Forms.Label date;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Button pay;
+        private System.Windows.Forms.TextBox tendered;
+        private System.Windows.Forms.Label required;
+        private System.Windows.Forms.Button process;
+        private System.Windows.Forms.Button cancel;
     }
 }
 
--- WInFormsMockPOS/Form1.cs
+++ WInFormsMockPOS/Form1.cs
@@ -13,13 +13,39 @@ namespace WInFormsMockPOS
     public partial class Form1 : Form
     {
         Employee[] user = new Employee[5];
+        Product[] items = new Product[6];
+        DataTable table = new DataTable();
+        DataTable table2 = new DataTable();
+        Employee currentUser;
+        double runningTotal = 0.00;
 
         public Form1()
         {
             updateName();
+            updateProducts();
             InitializeComponent();
-            username.Text = user[new Random().Next(0,5)].getFirstName();
+            currentUser = user[new Random().Next(0, 5)];
+            username.Text = currentUser.getFirstName();
             timer1.Start();
+            createTableHeaders();
+        }
+
+        private void createTableHeaders()
+        {
+            table.Columns.Add(new DataColumn("Description", typeof(String)));
+            table.Columns.Add(new DataColumn("Price", typeof(double)));
+            table.Columns.Add(new DataColumn("Quantity", typeof(int)));
+            table.Columns.Add(new DataColumn("Total", typeof(double)));
+            //dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.DataSource = table.DefaultView;
+
+            table2.Columns.Add(new DataColumn("Stamp", typeof(String)));
+            table2.Columns.Add(new DataColumn("User", typeof(String)));
+            table2.Columns.Add(new DataColumn("Total", typeof(double)));
+            table2.Columns.Add(new DataColumn("Tendered", typeof(double)));
+            table2.Columns.Add(new DataColumn("Change", typeof(double)));
+
+            dataGridView2.DataSource = table2.DefaultView;
 
         }
 
@@ -46,20 +72,142 @@ namespace WInFormsMockPOS
         private void updateProducts()
         {
             String[] products = { "Asiago", "Chedder", "Parmesan", "Swiss", "American", "PepperJack" };
-            double[] prices = { 4.99, 3.20, 8.56, 5.25, 4.32, 7.99 };
+            double[] prices = { .99, 1.20, .50, 1.25, 1.99, 1.12 };
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                items[i] = new Product(products[i], prices[i]);
+            }
         }
 
         private void username_Click(object sender, EventArgs e)
         {
-            Random rnd = new Random();
 
-            username.Text = user[rnd.Next(0, 5)].getFirstName(); 
+            Random rnd = new Random();
+            currentUser = user[new Random().Next(0, 5)];
+            username.Text = currentUser.getFirstName();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             date.Text = DateTime.Now.ToLongTimeString();
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            Product newItem = items[rnd.Next(0, items.Length)];
+            int quantity = rnd.Next(1, 4);
+            double total = (double)quantity * newItem.getPrice();
+
+            table.Rows.Add(new Object[] {newItem.getProductName(), newItem.getPrice(), quantity, total});
+            runningTotal += total;
+
+            // attempting to format double in to "money" x.xx
+            totalLabel.Text = String.Format("{0:#.00}", runningTotal.ToString());
+            updateRequired();
+        }
+
+        private void updateRequired()
+        {
+            required.Text = "required: " + (0.0 - runningTotal).ToString();
+            required.ForeColor = System.Drawing.Color.Red;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            add.Enabled = false;
+            clear.Enabled = false;
+            pay.Enabled = false;
+            tendered.Visible = true;
+            required.Visible = true;
+            process.Visible = true;
+            cancel.Visible = true;
+
+            updateRequired();
+        }
+
+        private void updateTenderedRequired()
+        {
+            updateRequired();
+
+            try
+            {
+                double currentEntered = Convert.ToDouble(tendered.Text);
+                if (runningTotal - currentEntered == 0)
+                {
+                    required.ForeColor = System.Drawing.Color.Black;
+                    required.Text = "change: " + 0;
+                    process.Enabled = true;
+                }
+                else if (runningTotal - currentEntered > 0)
+                {
+                    required.Text = "required: " + (runningTotal - currentEntered);
+                    process.Enabled = false;
+                }
+                else if (runningTotal - currentEntered < 0)
+                {
+                    required.ForeColor = System.Drawing.Color.Black;
+                    required.Text = "change: " + Math.Abs(runningTotal - currentEntered);
+                    process.Enabled = true;
+                }
+
+            }
+            catch (Exception exp)
+            {
+                //MessageBox.Show(exp.ToString());
+            }
+        }
+
+        private void tendered_TextChanged(object sender, EventArgs e)
+        {
+            updateTenderedRequired();
+        }
+
+        private void process_Click(object sender, EventArgs e)
+        {
+            String stamp = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+            double tenderEntered = Convert.ToDouble(tendered.Text);
+
+
+            table2.Rows.Add(new Object[] {stamp, currentUser.getFirstName(), runningTotal, tenderEntered, Math.Abs(runningTotal-tenderEntered)});
+
+            runningTotal = 0;
+            totalLabel.Text = "0.00";
+            tendered.Text = "0.00";
+            required.Visible = false;
+            process.Visible = false;
+            cancel.Visible = false;
+
+            //dataGridView1.DataSource = null;
+            table.Rows.Clear();
+            //dataGridView1.Refresh();
+            //dataGridView1.DataSource = table.DefaultView;
+
+            add.Enabled = true;
+            clear.Enabled = true;
+            pay.Enabled = true;
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            table.Rows.Clear();
+            runningTotal = 0;
+            totalLabel.Text = "0.00";
+
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            process.Visible = false;
+            cancel.Visible = false;
+            tendered.Visible = false;
+            required.Visible = false;
+            pay.Enabled = false;
+            add.Enabled = true;
+            clear.Enabled = true;
         }
     }
 }
